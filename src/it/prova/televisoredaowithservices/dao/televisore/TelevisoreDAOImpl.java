0ecfa8953a94e7bb0584fa57d872f5e4502dadc5
NoemiSpurio <noemi.spurio@gmail.com>
2022-10-22 16:25:15 +0200

    fix unused import

--- src/it/prova/televisoredaowithservices/dao/televisore/TelevisoreDAOImpl.java
+++ src/it/prova/televisoredaowithservices/dao/televisore/TelevisoreDAOImpl.java
@@ -3,7 +3,6 @@ package it.prova.televisoredaowithservices.dao.televisore;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
